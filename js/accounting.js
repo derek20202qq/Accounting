a91210a42adb9ee777ef7a7e291f146ca4b555f5
derek20202qq <derek20202@gmail.com>
2017-10-22 14:47:48 +0800

    Add files via upload

---
+++ js/accounting.js
@@ -0,0 +1,20 @@
+var fdb = new ForerunnerDB();
+var db = fdb.db("accounting");
+var accountingCollection = db.collection('accounting');
+accountingCollection.load();
+
+function addInfo(){
+  var newAccounting = {
+    date: $("#date").val(),
+    type: $("#type").val(),
+    item: $("#item").val(),
+    cost: $("#cost").val()
+  };
+  accountingCollection.insert(newAccounting);
+  accountingCollection.save()
+  $("#date").val("");
+  $("#type").val("");
+  $("#item").val("");
+  $("#cost").val("");
+  alert("Success");
+}
---
+++ js/recent10.js
@@ -0,0 +1,20 @@
+var fdb = new ForerunnerDB();
+var db = fdb.db("accounting");
+var accountingCollection = db.collection('accounting');
+
+accountingCollection.load(function(){
+    var accountings = accountingCollection.find(
+        {},
+        {$orderBy:{date:-1}},
+        {$limit: 10}
+    );
+
+    for(var i = 0; i < accountings.length; i++){
+      var date = accountings[i].date;
+      var type = accountings[i].type;
+      var item = accountings[i].item;
+      var cost = accountings[i].cost;
+
+      $("#accountingTable").append("<tr><th>"+date+"</th><th>"+type+"</th><th>"+item+"</th><th>"+cost+"</th></tr>");
+    }
+});
