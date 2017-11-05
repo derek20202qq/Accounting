eed2d442b4c7ee44d92ce8c57b2ddd8215a324d3
derek20202qq <derek20202@gmail.com>
2017-11-05 15:06:45 +0800

    Update accounting.js

--- js/accounting.js
+++ js/accounting.js
@@ -3,18 +3,18 @@ var db = fdb.db("accounting");
 var accountingCollection = db.collection('accounting');
 accountingCollection.load();
 
-function addInfo(){
-  var newAccounting = {
-    date: $("#date").val(),
-    type: $("#type").val(),
-    item: $("#item").val(),
-    cost: $("#cost").val()
-  };
-  accountingCollection.insert(newAccounting);
-  accountingCollection.save()
-  $("#date").val("");
-  $("#type").val("");
-  $("#item").val("");
-  $("#cost").val("");
-  alert("Success");
+function addInfo() {
+    var newAccounting = {
+        date: $("#date").val(),
+        type: $("#type").val(),
+        item: $("#item").val(),
+        cost: $("#cost").val()
+    };
+    accountingCollection.insert(newAccounting);
+    accountingCollection.save()
+    $("#date").val("");
+    $("#type").val("");
+    $("#item").val("");
+    $("#cost").val("");
+    alert("Success");
 }
